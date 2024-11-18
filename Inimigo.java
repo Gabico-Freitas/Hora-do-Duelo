f564c60c0bbc4a953ceceb889c51141cb9d81c1c
Gabico-Freitas <gabriel.f.silva278@gmail.com>
2024-11-18 08:46:51 -0300

    Update Inimigo.java

--- Inimigo.java
+++ Inimigo.java
@@ -1,7 +1,7 @@
 class Inimigo extends Personagem{
     private String corpo;
-    public Inimigo(String nome, int vida, String corpo){
-        super(nome, vida);
+    public Inimigo(String nome, int vida, String corpo, int dano){
+        super(nome, vida, dano);
         this.corpo=corpo;
     }
     public String getCorpo(){
